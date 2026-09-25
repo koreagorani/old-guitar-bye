ba343a58b7cb09700470faefae5d5e5e21f6a773
koreagorani <gunbo813@gmail.com>
2026-09-25 19:22:56 +0900

    Add inventory edit menu renderer

---
+++ src/telegram/render/inventory-edit-menu-renderer.js
@@ -0,0 +1,11 @@
+const EDIT_ACTIONS = Object.freeze([
+  Object.freeze({ id: "edit_purchase_price", label: "매입가 수정" }),
+  Object.freeze({ id: "edit_expected_sale_price", label: "예상 판매가 수정" }),
+  Object.freeze({ id: "back", label: "뒤로" }),
+]);
+
+export function renderInventoryEditMenu() {
+  return {
+    primaryActions: EDIT_ACTIONS.map((action) => ({ ...action })),
+  };
+}
